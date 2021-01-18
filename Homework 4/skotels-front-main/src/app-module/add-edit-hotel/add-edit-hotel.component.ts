ed0f0a07cd7c8d50320780f7242a42b6447f1774
Robert Ilievski <rilievski54@gmail.com>
2021-01-19 00:16:09 +0100

    Created Homework 4 folder and added the frontend

---
+++ Homework 4/skotels-front-main/src/app-module/add-edit-hotel/add-edit-hotel.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {HotelsServiceService} from '../../services/hotels-service.service';
+
+@Component({
+  selector: 'app-add-edit-hotel',
+  templateUrl: './add-edit-hotel.component.html',
+  styleUrls: ['./add-edit-hotel.component.css']
+})
+export class AddEditHotelComponent implements OnInit {
+  hotelsFG: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router, private hotelService: HotelsServiceService,
+              private toastr: ToastrService) {
+    this.hotelsFG = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', []],
+      location: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      internetAccess: ['', []],
+      rooms: ['', []],
+      phone: ['', []],
+      website: ['', []],
+      stars: ['', [Validators.required]],
+      latitude: ['', []],
+      longitude: ['', []],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  discard(): void {
+    this.router.navigate(['/hotels']);
+  }
+  // tslint:disable-next-line:typedef
+  async submitHotel() {
+    const data = this.hotelsFG.getRawValue();
+    await this.hotelService.add(data).subscribe();
+    await this.router.navigate(['/hotels']);
+    this.toastr.success('You successfully added a hotel!');
+  }
+
+}
---
+++ Homework 4/skotels-front-main/src/models/hotels.model.ts
@@ -0,0 +1,15 @@
+export class HotelsModel {
+  public location: string;
+  public _id: string;
+  public name: string;
+  public email: string;
+  public fax: string;
+  public internetAccess: string;
+  public rooms: string;
+  public phone: number;
+  public website: string;
+  public stars: string;
+  public latitude: string;
+  public longitude: string;
+  public address: string;
+}
