6f06c36d81adbdff26eaebc86670bcbc53ebd7ce
Robert Ilievski <rilievski54@gmail.com>
2021-01-24 21:58:49 +0100

    Implemented login, register, logout with spring security

--- Homework 3 & 4/skotels-backend/src/main/java/skotels/hotelapp/model/User.java
+++ Homework 3 & 4/skotels-backend/src/main/java/skotels/hotelapp/model/User.java
@@ -5,7 +5,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Data
 @Document
 public class Users {
